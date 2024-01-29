d6f2bb397d939579bd031b759236ef479071d41e
root <root@ip-172-30-2-64.us-west-2.compute.internal>
2024-01-29 20:37:48 +0000

    working POC

---
+++ main.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"context"
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/sts"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/x/mongo/driver/auth"
+)
+
+// $ export AWS_ACCESS_KEY_ID=<access_id>
+// $ export AWS_SECRET_ACCESS_KEY=<secret_key>
+// $ export AWS_DEFAULT_REGION=us-west-2
+// $ export DOCDB_URL=<docdb_url>
+
+func main() {
+	ctx := context.TODO()
+
+	printAWSIdentity(ctx)
+	connectWithAWSIdentity(ctx)
+}
+
+func printAWSIdentity(ctx context.Context) {
+	cfg, err := config.LoadDefaultConfig(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	stsService := sts.NewFromConfig(cfg)
+	identity, err := stsService.GetCallerIdentity(ctx, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("== AWS caller identity")
+	fmt.Println("ARN: ", aws.ToString(identity.Arn))
+}
+
+func connectWithAWSIdentity(ctx context.Context) {
+	fmt.Println("== Connect", os.Getenv("DOCDB_URL"))
+	options := options.Client().
+		ApplyURI("mongodb://" + os.Getenv("DOCDB_URL")).
+		SetDirect(true).
+		SetAuth(makeCred()).
+		SetTLSConfig(makeTLSConfig())
+
+	fmt.Println("Preparing...")
+	client, err := mongo.Connect(ctx, options)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected.")
+
+	result := client.Database("test").Collection("users").FindOne(ctx, bson.D{})
+	if result.Err() != nil {
+		log.Fatal(err)
+	}
+
+	var data = struct {
+		Name string
+		Age  int
+	}{}
+
+	if err := result.Decode(&data); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Result: ", data)
+}
+
+func makeCred() options.Credential {
+	cred := options.Credential{
+		AuthMechanism: auth.MongoDBAWS,
+		AuthSource:    "$external",
+		Username:      os.Getenv("AWS_ACCESS_KEY_ID"),
+		Password:      os.Getenv("AWS_SECRET_ACCESS_KEY"),
+	}
+	if token := os.Getenv("AWS_SESSION_TOKEN"); token != "" {
+		cred.AuthMechanismProperties = map[string]string{
+			"AWS_SESSION_TOKEN": token,
+		}
+	}
+	return cred
+}
+
+func makeTLSConfig() *tls.Config {
+	cas, err := os.ReadFile("global-bundle.pem")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pool := x509.NewCertPool()
+	pool.AppendCertsFromPEM(cas)
+	return &tls.Config{
+		RootCAs: pool,
+	}
+}
